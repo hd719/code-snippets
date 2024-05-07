91d18a3caeee86058bbe58e8d303487df6eef032
Hamel Desai <hameldesai93@gmail.com>
2024-05-07 15:12:00 -0400

    section-5-video-42

--- actions/index.ts
+++ actions/index.ts
@@ -16,3 +16,31 @@ export async function deleteSnippet(id: number) {
   await db.snippet.delete({ where: { id } });
   redirect("/");
 }
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  return {
+    message: "This is a test message",
+  };
+  // // This needs to be a server action
+  // // "use server"; // This is a server action, it will be run on the server
+
+  // // Check the user's input and make sure its valid
+  // const title = formData.get("title") as string;
+  // const code = formData.get("code") as string;
+
+  // // Take the user input and create a new record in the db
+  // const snippet = await db.snippet.create({
+  //   data: {
+  //     title,
+  //     code,
+  //   },
+  // });
+
+  // console.log("Created new snippet", snippet);
+
+  // // Redirect the user to the new snippet's page
+  // redirect("/");
+}
--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,31 +1,15 @@
-import { db } from "@/db";
-import { redirect } from "next/navigation";
+"use client";
+
+import { useFormState } from "react-dom";
+import * as actions from "@/actions";
 
 export default function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    // This needs to be a server action
-    "use server"; // This is a server action, it will be run on the server
-
-    // Check the user's input and make sure its valid
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-
-    // Take the user input and create a new record in the db
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-
-    console.log("Created new snippet", snippet);
-
-    // Redirect the user to the new snippet's page
-    redirect("/");
-  }
+  const [formState, action] = useFormState(actions.createSnippet, {
+    message: "",
+  });
 
   return (
-    <form action={createSnippet} className="">
+    <form action={action} className="">
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -45,6 +29,7 @@ export default function SnippetCreatePage() {
             className="border rounded p-2 w-full"
           />
         </div>
+        <div>{formState.message}</div>
         <button type="submit" className="border rounded p-2 bg-blue-200">
           Create
         </button>
