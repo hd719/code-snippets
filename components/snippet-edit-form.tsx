08f055662723b74b0e174cf3e542acdeef0c7480
Hamel Desai <hameldesai93@gmail.com>
2024-05-07 14:22:10 -0400

    section-3-video-34

--- components/snippet-edit-form.tsx
+++ components/snippet-edit-form.tsx
@@ -1,11 +1,24 @@
 "use client";
 
 import type { Snippet } from "@prisma/client";
+import Editor from "@monaco-editor/react";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
-  return <div>client</div>;
+  return (
+    <div>
+      <Editor
+        height="40vh"
+        defaultLanguage="javascript"
+        defaultValue={snippet.code}
+        theme="vs-dark"
+        options={{
+          minimap: { enabled: false },
+        }}
+      />
+    </div>
+  );
 }
