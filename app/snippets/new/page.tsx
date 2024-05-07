ee6e444923b0fa9926e0f60897cd12cb2033df65
Hamel Desai <hameldesai93@gmail.com>
2024-05-07 11:09:20 -0400

    section-2-video-21

--- app/snippets/new/page.tsx
+++ app/snippets/new/page.tsx
@@ -1,3 +1,29 @@
 export default function SnippetCreatePage() {
-  return <div>new page</div>;
+  return (
+    <form action="" className="">
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label htmlFor="code">Code</label>
+          <textarea
+            id="code"
+            name="code"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+        <button type="submit" className="border rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
 }
